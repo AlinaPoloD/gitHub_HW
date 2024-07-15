f9f9027d7b951e78537be18f6ba6195c97e16c55
Alina <alinapolodmi@mail.ru>
2024-07-15 23:35:57 +0200

    new correct 1

--- HWBuble.java
+++ HWBuble.java
@@ -1,91 +1,92 @@
-//package HW;
-//
-//import java.io.File;
-//import java.io.FileWriter;
-//import java.io.FileReader;
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.util.Arrays;
-//import java.text.SimpleDateFormat;
-//import java.util.Date;
-//import java.util.logging.FileHandler;
-//import java.util.logging.Logger;
-//import java.util.logging.SimpleFormatter;
-//
-////После каждого прохода по массиву ваш код должен делать запись в лог-файл 'log.txt'
-////в формате год-месяц-день час:минуты {массив на данной итерации}.
-////Для логирования использовать логгер logger класса BubbleSort.
-//class BubbleSort {
-//    private static File log;
-//    private static FileWriter fileWriter;
-//
-//
-//    public static void sort(int[] mas) throws IOException {
-//            for (int i = 0; i < mas.length; i++) {
-//                for (int j = 0; j < mas.length - 1; j++) {
-//                    if (mas[j] > mas[j + 1]) {
-//                        int temp = mas[j];
-//                        mas[j] = mas[j + 1];
-//                        mas[j + 1] = temp;
-//
-//                    }
-//
-//
-//                }
-//                writeArrToFile(mas,);
-//
-//
-//            }
-//
-//        }
-//
-//
-//
-//
-//
-//    static void writeArrToFile(int[] dataArr, String fileName) {
-//
-//        try (FileWriter fw = new FileWriter(fileName)) {
-//            for (Integer num : dataArr) {
-//
-//                //в формате год-месяц-день час:минуты {массив на данной итерации}.
-//                fw.write(Integer.toString(num));
-//                fw.append(System.lineSeparator());
-//            }
-//            fw.flush();
-//            logger.info("Data saved!");
-//        } catch (IOException e) {
-//            logger.warning(e.getMessage());
-//// logger.log(Level.WARNING, e.getMessage());
-//        }
-//    }
-//
-//}
-//
-//// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
-//public class HWBuble {
-//    public static void main(String[] args) throws IOException {
-//        int[] arr = {};
-//
-//        if (args.length == 0) {
-//            // При отправке кода на Выполнение, вы можете варьировать эти параметры
-//            arr = new int[]{9, 4, 8, 3, 1};
-//        } else {
-//            arr = Arrays.stream(args[0].split(", "))
-//                    .mapToInt(Integer::parseInt)
-//                    .toArray();
-//        }
-//
-//        BubbleSort ans = new BubbleSort();
-//        ans.sort(arr);
-//
-//        try (BufferedReader br = new BufferedReader(new FileReader("log.txt"))) {
-//            String line;
-//            while ((line = br.readLine()) != null) {
-//                System.out.println(line);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+pa ckage HW;
+
+im port java.io.File;
+im port java.io.FileWriter;
+im port java.io.FileReader;
+im port java.io.BufferedReader;
+im port java.io.IOException;
+im port java.util.Arrays;
+im port java.text.SimpleDateFormat;
+im port java.util.Date;
+im port java.util.logging.FileHandler;
+im port java.util.logging.Logger;
+ 
+//После каждого прохода по массиву ваш код должен делать запись в лог-файл 'log.txt'
+//в  формате год-месяц-день час:минуты {массив на данной итерации}.
+// 
+//Дл я логирования использовать логгер logger класса BubbleSort.
+clas s BubbleSort {
+    private static File log;
+   vate static FileWriter fileWriter;
+ 
+
+   public static void sort(int[] mas) throws IOException {
+        for (int i = 0; i < mas.length; i++) {
+    for (int j = 0; j < mas.length - 1; j++) {
+    if (mas[j] > mas[j + 1]) {
+    int temp = mas[j];
+   [j] = mas[j + 1];
+   [j + 1] = temp;
+ 
+                   }
+ 
+
+               }
+   teArrToFile(mas,);
+ 
+
+           }
+ 
+       }
+ 
+
+
+
+
+   static void writeArrToFile(int[] dataArr, String fileName) {
+ 
+       try (FileWriter fw = new FileWriter(fileName)) {
+    for (Integer num : dataArr) {
+ 
+               //в формате год-месяц-день час:минуты {массив на данной итерации}.
+   write(Integer.toString(num));
+   append(System.lineSeparator());
+   
+   flush();
+   ger.info("Data saved!");
+    (IOException e) {
+    logger.warning(e.getMessage());
+// Level.WARNING, e.getMessage());
+       }
+   
+ 
+}
+ 
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class HWBuble {
+// 
+    public static void main(String[] args) throws IOException {
+    int[] arr = {};
+ 
+       if (args.length == 0) {
+    // При отправке кода на Выполнение, вы можете варьировать эти параметры
+    = new int[]{9, 4, 8, 3, 1};
+   {
+    arr = Arrays.stream(args[0].split(", "))
+        .mapToInt(Integer::parseInt)
+   Array();
+   
+ 
+       BubbleSort ans = new BubbleSort();
+   .sort(arr);
+ 
+       try (BufferedReader br = new BufferedReader(new FileReader("log.txt"))) {
+    String line;
+   le ((line = br.readLine()) != null) {
+    System.out.println(line);
+   
+    (IOException e) {
+    e.printStackTrace();
+   
+   
+}  
